9d811a28ecb80c70106626e57f956039cffa353d
Emerson Souza <emersouza95@gmail.com>
2024-10-18 13:07:12 -0300

    alteracao

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,21 +1,21 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (formParams = {}, validateNameBeforeClick = false) => {
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (customFormParams = {}, validateFormField = false) => {
     const defaultFormParams = {
         firstName: 'John',
         lastName: 'Doe',
-        email: 'test@fakeemail.com',
+        email: 'test@fakemail.com',
         textArea: 'Amazing work!'
     }
 
-    const finalFormParams = { ...defaultFormParams, ...formParams }
+    const finalFormParams = { ...defaultFormParams, ...customFormParams }
 
     cy.get('#firstName').as('firstName').type(finalFormParams.firstName)
     cy.get('#lastName').as('lastName').type(finalFormParams.lastName)
     cy.get('#email').type(finalFormParams.email)
     cy.get('#open-text-area').type(finalFormParams.textArea)
 
-    if (validateNameBeforeClick) {
-        cy.get('@firstName').should('have.value', formParams.firstName)
-        cy.get('@lastName').should('have.value', formParams.lastName)
+    if (validateFormField) {
+        cy.get('@firstName').should('have.value', customFormParams.firstName)
+        cy.get('@lastName').should('have.value', customFormParams.lastName)
     }
     cy.contains('button', 'Send').should('be.enabled').click()
 })
